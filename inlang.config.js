1fa9c68f2ee0fd39a3eb7921d08122d9074b8a4d
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-02-27 22:36:22 +0100

    .

---
+++ inlang.config.js
@@ -0,0 +1,16 @@
+export async function defineConfig(env) {
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+  )
+
+  const pluginConfig = {
+    pathPattern: "./{language}.json",
+  }
+
+  return {
+    referenceLanguage: "en",
+    languages: ["en", "de"],
+    readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),,
+    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
+  }
+}
